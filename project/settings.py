7bad2ed6fabd177fccf0a894e23389b23697f1ff
Epicgika <Taher.sanad2000@gmail.com>
2024-11-14 19:35:18 +0200

    Last update

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
-	'product.apps.ProductConfig',
+	'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
