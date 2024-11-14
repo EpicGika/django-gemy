170c048bb98cd88a36373179f156246539957b2e
Epicgika <Taher.sanad2000@gmail.com>
2024-11-14 18:12:34 +0200

    Push django project

---
+++ pages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+# Create your views here.
+
+
+def index(request):
+
+    return render(request, 'pages/index.html',)
+
+def about(request):
+	x = {
+        'name' : 'taher',
+        'age' : 135456465425,
+    }
+	return render(request, 'pages/about.html', x)
+	
